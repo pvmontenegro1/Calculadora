c0df7f06db4586f4c7e3fd736393c6f897f89de3
pvmontenegro1 <105465370+pvmontenegro1@users.noreply.github.com>
2024-06-29 23:25:19 -0500

    Add files via upload

---
+++ SpringBootSoap/SpringBootSoap/src/main/java/com/soap/SpringBootSoapApplication.java
@@ -0,0 +1,13 @@
+package com.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapApplication {
+
+	public static vmnvoid main(String[] args) {
+		SpringApplication.run(SpringBootSoapApplication.class, args);
+	}
+
+}
